24e13b00b2a84eaeda46d9a9c71a91bec10058c1
Montalvas <62609007+montalvas@users.noreply.github.com>
2020-08-07 13:54:23 -0300

    Finishing

---
+++ src/database/fake_data.js
@@ -0,0 +1,26 @@
+// Dados
+const proffys = [
+    { 
+        name: "Diego Fernandes", 
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "8989898989",
+        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "20",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1220] 
+    },
+
+    { 
+        name: "Lucas Montalvani", 
+        avatar: "https://avatars1.githubusercontent.com/u/62609007?s=460&u=f26200c0b90b7e6a26eb50e5361f9098b1562e83&v=4",
+        whatsapp: "1221122112",
+        bio: "A minha arte é apenas o rascunho da minha própria vida.<br><br>A melhor maneira de nos prepararmos para o futuro é concentrar toda a imaginação e entusiasmo na execução perfeita do trabalho de hoje.",
+        subject: "Artes",
+        cost: "100",
+        weekday: [1],
+        time_from: [720],
+        time_to: [1220] 
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,109 +1,20 @@
-// Dados
-const proffys = [
-    { 
-        name: "Diego Fernandes", 
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-        whatsapp: "8989898989",
-        bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-        subject: "Química",
-        cost: "20",
-        weekday: [0],
-        time_from: [720],
-        time_to: [1220] 
-    },
+const express = require('express');
+const server = express();
 
-    { 
-        name: "Lucas Montalvani", 
-        avatar: "https://avatars1.githubusercontent.com/u/62609007?s=460&u=f26200c0b90b7e6a26eb50e5361f9098b1562e83&v=4",
-        whatsapp: "1221122112",
-        bio: "A minha arte é apenas o rascunho da minha própria vida.<br><br>A melhor maneira de nos prepararmos para o futuro é concentrar toda a imaginação e entusiasmo na execução perfeita do trabalho de hoje.",
-        subject: "Artes",
-        cost: "100",
-        weekday: [1],
-        time_from: [720],
-        time_to: [1220] 
-    }
-]
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
 
-const subjects = [
-    "Artes", 
-    "Biologia", 
-    "Ciências", 
-    "Educação física",
-    "Física", 
-    "Geografia", 
-    "História", 
-    "Matemática", 
-    "Português",
-    "Química",
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda-feira",
-    "Terça-feira",
-    "Quarta-feira",
-    "Quinta-feira",
-    "Sexta-feira",
-    "Sábado",
-]
-
-// Funcionalidades
-
-function getSubject(subjectNumber) {
-    const position = +subjectNumber - 1
-    return subjects[position]
-}
-
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-    //adicionar data a lista de proffys
-    const isNotEmpty = Object.keys(data).length != 0
-
-    if (isNotEmpty){
-
-        data.subject = getSubject(data.subject)
-
-        proffys.push(data)
-
-        return res.redirect("/study")
-    }
-
-    return res.render("give-classes.html", { subjects, weekdays })
-}
-
-
-//Servidor
-const express = require('express')
-const server = express()
-
-//configurar nunjucks (template engine)
-const nunjucks = require('nunjucks')
+const nunjucks = require('nunjucks');
 nunjucks.configure('src/views', {
-    express: server,
-    noCache: true,
+  express: server,
+  noCache: true,
 })
 
-//Configuração do servidor
 server
-
-//configurar arquivos estáticos (css, scripts, imagens)
+.use(express.urlencoded({ extended: true }))
 .use(express.static("public"))
-
-//rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 
-//Start do servidor
-.listen(5500)
-
+.listen(5500);
